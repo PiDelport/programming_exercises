3f8fb6c3407ee6d20caeb49bfe86f341bc403242
ClothoMoirai <OhDaniGal@gmail.com>
2020-07-14 01:31:36 -0500

    Learned os.walk() and that greatly simplifed where I was going. Size-grouping is implemented and hard links are grouped as well.

--- file_duplicate_detection.py
+++ file_duplicate_detection.py
@@ -12,6 +12,7 @@
 
 import os, hashlib
 from typing import List
+from pprint import pprint as dataprint
 
 # temporary hardcoding during development
 basedir = '~/test'
@@ -36,14 +37,40 @@ def isdir(filename: str) -> bool:
         return False
 
 
-def scandirectory(path: str) -> List['str']:
-    pass
+def GetFileStats(filename: str) -> dict:
+    # Return a dict with the size and, if there are multiple links, the inode
+    stat_dict = {}
+    this_file_stat = os.stat(filename)
+    stat_dict['size'] = this_file_stat.st_size
+    if this_file_stat.st_nlink > 1:
+        stat_dict['inode'] = this_file_stat.st_ino
+    return stat_dict
+
+
+def ScanDirectory(path: str) -> dict:
+    files_dict = { 'size': {}, 'inodes': {}}
+    for this_path, these_dirs, these_files in os.walk(path, followlinks=False):
+        for this_file in these_files:
+            this_file_abs_name = this_path + '/' + this_file
+            if isfile(this_file_abs_name):
+                stats_dict = GetFileStats(this_file_abs_name)
+                if not stats_dict['size'] in files_dict['size'].keys():
+                    files_dict['size'][stats_dict['size']] = [this_file_abs_name]
+                else:
+                    files_dict['size'][stats_dict['size']].append(this_file_abs_name)
+                if 'inode' in files_dict.keys():
+                    if not stats_dict['inode'] in files_dict['inodes'].keys():
+                        files_dict['inodes'][stats_dict['inode']] = [this_file_abs_name]
+                    else:
+                        files_dict['inodes'][stats_dict['inode']].append(this_file_abs_name)
+    return files_dict
 
 
 def main(basedir: str) -> None:
-    basedir = os.path.expanduser(basedir)
+    basedir = os.path.expanduser(basedir) # gets the absolute path if given in a form like ~/mydir
     if isdir(basedir):
-        print("{} is a directory.".format(basedir))
+        possible_dup_dict = ScanDirectory(basedir)
+        dataprint(possible_dup_dict)
     else:
         print('{} is not a directory.'.format(basedir))
 
