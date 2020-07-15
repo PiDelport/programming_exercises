dbc1a3d930e3acec6781df21416ae35cd5f1ef6f
ClothoMoirai <OhDaniGal@gmail.com>
2020-07-15 01:10:29 -0500

    Now rewrites the inode key for later processing, hashes generated and grouped by hash value.

--- file_duplicate_detection.py
+++ file_duplicate_detection.py
@@ -10,12 +10,16 @@
 # Hash files
 # Compare hashes
 
-import os, hashlib
-from typing import List
+import hashlib
+import os
 from pprint import pprint as dataprint
+from typing import List
 
 # temporary hardcoding during development
 basedir = '~/test'
+# blocksize for hashing
+blocksize = 65536
+
 
 def islink(filename: str) -> bool:
     return os.path.islink(filename)
@@ -37,7 +41,7 @@ def isdir(filename: str) -> bool:
         return False
 
 
-def GetFileStats(filename: str) -> dict:
+def get_file_stats(filename: str) -> dict:
     # Return a dict with the size and, if there are multiple links, the inode
     stat_dict = {}
     this_file_stat = os.stat(filename)
@@ -47,13 +51,13 @@ def GetFileStats(filename: str) -> dict:
     return stat_dict
 
 
-def ScanDirectory(path: str) -> dict:
-    files_dict = { 'size': {}, 'inodes': {}}
+def scan_directory(path: str) -> dict:
+    files_dict = {'size': {}, 'inodes': {}}
     for this_path, these_dirs, these_files in os.walk(path, followlinks=False):
         for this_file in these_files:
             this_file_abs_name = this_path + '/' + this_file
             if isfile(this_file_abs_name):
-                stats_dict = GetFileStats(this_file_abs_name)
+                stats_dict = get_file_stats(this_file_abs_name)
                 if not stats_dict['size'] in files_dict['size'].keys():
                     files_dict['size'][stats_dict['size']] = [this_file_abs_name]
                 else:
@@ -66,12 +70,55 @@ def ScanDirectory(path: str) -> dict:
     return files_dict
 
 
-def main(basedir: str) -> None:
-    basedir = os.path.expanduser(basedir) # gets the absolute path if given in a form like ~/mydir
-    if isdir(basedir):
-        possible_dup_dict = ScanDirectory(basedir)
+def clear_single_entries(stat_sub_dict: dict) -> dict:
+    remove_keys = [key for key in stat_sub_dict if len(stat_sub_dict[key]) < 2]
+    for this_key in remove_keys:
+        stat_sub_dict.pop(this_key)
+    return stat_sub_dict
+
+
+def dict_values_to_list(source_dict: dict) -> List[List[str]]:
+    return [source_dict[key] for key in source_dict.keys() if len(source_dict[key]) > 1]
+
+
+def clean_stat_dict(files_dict: dict) -> dict:
+    # clean up the data before returning
+    files_dict['size'] = clear_single_entries(files_dict['size'])
+    files_dict['inodes'] = dict_values_to_list(files_dict['inodes'])
+    return files_dict
+
+
+def hash_this_file(file_list: List) -> dict:
+    file_hashes_dict = {}
+    for this_file in file_list:
+        hasher = hashlib.sha512()
+        with open(this_file,'rb') as read_this_file:
+            my_buffer = read_this_file.read(blocksize)
+            while len(my_buffer) > 0:
+                hasher.update(my_buffer)
+                my_buffer = read_this_file.read(blocksize)
+        if hasher.hexdigest() not in file_hashes_dict:
+            file_hashes_dict[hasher.hexdigest()] = [this_file]
+        else:
+            file_hashes_dict[hasher.hexdigest()].append(this_file)
+    return file_hashes_dict
+
+
+def get_file_hashes(file_size_dict: dict) -> dict:
+    file_hashes_dict = {}
+    for this_file in file_size_dict.values():
+        file_hashes_dict.update(hash_this_file(this_file))
+    return file_hashes_dict
+
+
+def main(mydir: str) -> None:
+    mydir = os.path.expanduser(mydir)  # gets the absolute path if given in a form like ~/mydir
+    if isdir(mydir):
+        possible_dup_dict = clean_stat_dict(scan_directory(mydir))
+        possible_dup_dict['hashes'] = get_file_hashes(possible_dup_dict['size'])
         dataprint(possible_dup_dict)
     else:
-        print('{} is not a directory.'.format(basedir))
+        print('{} is not a directory.'.format(mydir))
+
 
 main(basedir)
